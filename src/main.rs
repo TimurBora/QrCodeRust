f5275ae1347f972161127f21d60d7731a0bfe52b
TimurBora <timurborisov5561@gmail.com>
2023-07-01 18:55:26 +0200

    CREATE SEPARATORS

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,117 @@
 
+use std::ops::{IndexMut, Index};
 
-const SIZE_SCAN: i32 = 7;
+use::generic_matrix;
+use generic_matrix::Matrix;
 
-struct Module {
-    value: bool,
-    is_define: bool,
-    cordinate: (i32, i32),
+#[derive(Clone, Copy)]
+enum Module {
+    Unknown,
+    Reserved,
+    Data(bool),
+    Function(bool),
 }
 
 impl Module {
-    fn set_module(&mut self, value: bool) {
-        self.value = value;
-    }
+
 }
 
-struct Block {
-    width: i32,
-    height: i32,
-    matrix: Vec<Vec<Module>>,
-    value: bool,
+struct QrMatrix {
+    size: usize,
+    modules: Matrix<Module>,
 }
 
-impl Block {
-    fn new(width: i32, height: i32, value: bool, is_define: bool) -> Self {
-        let mut matrix: Vec<Vec<Module>> = Vec::new();
-
-        for i in 0..height {
-            let mut row: Vec<Module> = Vec::new();
-            for j in 0..width {
-                let mut module: Module = Module { value, is_define, cordinate: (i, j) };
-                row.push(module);
-            }
-
-            matrix.push(row);
-        }
-
-        let mut block = Block { width: width, height: height, value: value, matrix: matrix };
-
-        return block;
+impl QrMatrix {
+    fn new(size: usize) -> Self {
+        QrMatrix { size: size, modules: Matrix::from_vec(size, size, vec![Module::Unknown; size * size]) }
     }
 
-    fn print_block(&self) {
-        for row in self.matrix.iter() {
-            for element in row
-            {
-                print!("{}", if element.value { "1" } else { "0" });
+    fn set_rect(&mut self, cordinate1: (usize, usize), cordinate2: (usize, usize), module: Module) {
+        let start_width: usize = cordinate1.1;
+        let start_height: usize = cordinate1.0;
+
+        let width: usize = get_width(&cordinate1, &cordinate2);
+        let height: usize = get_height(&cordinate1, &cordinate2);
+
+        for i in start_height..height {
+            for j in start_width..width {
+                let mut _module: &mut Module = self.modules.index_mut((i, j));
+                *_module = module;
             }
-            println!();
         }
     }
-}
 
-struct ScanBlock {
-    block: Block,
-}
+    fn set_square(&mut self, size: usize, cordinate: (usize, usize), module: Module) {
+        let start_width: usize = cordinate.1;
+        let start_height: usize = cordinate.0;
 
-impl ScanBlock {
-    fn new() -> Self {
-        let block: Block = Block::new(SIZE_SCAN, SIZE_SCAN, true, true);
-
-        let scan_block: ScanBlock = ScanBlock { block: block };
-
-        return scan_block;
+        for i in start_height..size {
+            for j in start_width..size {
+                let mut _module: &mut Module = self.modules.index_mut((i, j));
+                *_module = module;
+            }
+        }
     }
 
-    fn shaping(&self) {
-        let mut scan_matrix: &Vec<Vec<Module>> = &self.block.matrix;
-        
-        for row in scan_matrix.iter() {
-            for element in row {
-                if element.cordinate.0 == 1 || element.cordinate.0 == 5 || element.cordinate.1 == 1 || element.cordinate.1 == 5 {
-                    element.set_module(false);
+    fn print_matrix(&self) {
+        for i in 0..self.size {
+            for j in 0..self.size {
+                match self.modules.index((i, j)) {
+                    Module::Unknown => print!("0"),
+                    _ => print!(" "),
                 }
             }
+            println!("");
         }
     }
 }
 
-
-struct QrCode {
-    main_matrix: Block,
-    main_cordinate: Vec<Vec<(i32, i32)>>,
+struct QrBuilder {
+    matrix: QrMatrix,   
 }
 
+impl QrBuilder {
+    fn create_finder() -> Vec<Module> {
+        let mut finder_matrix: Matrix<Module> = Matrix::from_vec(7, 7, vec![Module::Function(true); 49]);
+        let mut finder_vector: Vec<Module>;
+
+        for i in 0..finder_matrix.row() {
+            for j in 0..finder_matrix.column() {
+                if i == 1 || i == 5 || j == 1 || j == 5 {
+                    let mut element: Module = *finder_matrix.index_mut((i, j));
+                    element = Module::Function(false);
+                }
+                finder_vector.push(*finder_matrix.index((i, j)));
+            }
+        }
+
+        return finder_vector;
+    }
+
+    fn add_finder(&mut self) -> Matrix<Module> {
+        let finder_vector: Vec<Module> = QrBuilder::create_finder();
+        let cordinate_vec: Vec<(usize, usize)> = vec![(0, 0),
+                                                      (0, self.matrix.modules.column() - 7), 
+                                                      (self.matrix.modules.row() - 7, 0)];
+        
+    }
+}
+
+fn get_width(cordinate1: &(usize, usize), cordinate2: &(usize, usize)) -> usize {
+    let width: usize = cordinate2.1 - cordinate1.1 + 1;
+
+    return width;
+}
+
+fn get_height(cordinate1: &(usize, usize), cordinate2: &(usize, usize)) -> usize {
+    let height: usize = cordinate2.0 - cordinate1.0 + 1 ;
+
+    return height;
+}
+
+
 fn main() {
-
-    let scan_block1: ScanBlock = ScanBlock::new();
-    scan_block1.block.print_block();
-
-
+    let mut matrix: QrMatrix = QrMatrix::new(7);
+    matrix.set_rect((0, 0), (3, 1), Module::Data(true));
+    matrix.print_matrix();
 }
