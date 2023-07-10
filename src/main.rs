1fd65e56c277d97bb5549b76385ebb2f5649e866
TimurBora <timurborisov5561@gmail.com>
2023-07-10 18:40:57 +0200

    Ok Just

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use qr_matrix::QrMatrix;
 use finder_builder::FinderBuilder;
 use timing_builder::TimingBuilder;
 use data_mode::Mode;
-use numeric_data_encoding::{NumericGroups};
 
 fn get_width(cordinate1: &(usize, usize), cordinate2: &(usize, usize)) -> usize {
     let width: usize = cordinate2.1 - cordinate1.1 + 1;
@@ -38,9 +37,6 @@ fn main() {
 
     let mode: Mode = Mode::Byte;
     mode.add_mode_to_qr_matrix(&mut qr_block);
-
-    let numeric: NumericGroups = NumericGroups::new("12371231249124810924890182490182409182490812947129875489175917501384901823901823981239081293192371289371289389123894".to_string());
-    numeric.break_string_to_group();
 
     qr_block.print_matrix();
 }
--- src/numeric_data_encoding.rs
+++ src/numeric_data_encoding.rs
@@ -1,20 +1,20 @@
 
 
-use bitvec::vec;
+use bitvec::prelude::*;
 
 use crate::data_mode::Mode;
 
-pub struct NumericGroups {
+struct NumericGroups {
     data: String,
 }
 
 impl NumericGroups {
-    pub fn new(data: String) -> Self {
+    fn new(data: String) -> Self {
         return NumericGroups {data: data};
     }
 
-    pub fn break_string_to_group(&self) -> Vec<u16> {
-        let mut data: &String = self.get_data();
+    fn break_string_to_group(&self) -> Vec<u16> {
+        let data: &String = self.get_data();
         let mut result_vector: Vec<u16> = Vec::new();
 
         for i in 0..data.len() / 3 {
@@ -23,7 +23,9 @@ impl NumericGroups {
         }
 
         if data.len() % 3 != 0 {
-            result_vector.push(self.get_last_elements(data).unwrap());
+            let num_last_elements: usize = data.len() % 3;
+            let last_elements_vector: String = data[data.len() - num_last_elements..].to_string();
+            result_vector.push(self.convert_string_to_int(last_elements_vector));
         }
         
         return result_vector;
@@ -34,16 +36,38 @@ impl NumericGroups {
         let integer_data: u16 = string_data.parse().unwrap();
         return integer_data;
     }
-    
-    fn get_last_elements(&self, data: &String) -> Option<u16> {
-        match data.len() % 3 {
-            1 => return Some(self.convert_string_to_int(data[data.len() - 1..].to_string())),
-            2 => return Some(self.convert_string_to_int(data[data.len() - 2..].to_string())),
-            _ => None,
-        }
-    }
 
     fn get_data(&self) -> &String {
         return &self.data;
+    }
+}
+
+struct NumericToBinaryConverter {
+    integer_group_vector: Vec<u16>,
+}
+
+impl NumericToBinaryConverter {
+    fn new(data: String) -> Self {
+        let group_vector: Vec<u16> = NumericToBinaryConverter::get_group_vector(data);
+        return NumericToBinaryConverter { integer_group_vector: group_vector };
+    }
+
+    // fn get_binary_vectors(&self) -> Vec<BitVec<u16, Msb0>> {
+
+    // }
+
+    fn generate_bitvec(binary: u16) -> BitVec<u16, Msb0> {
+        let bitvector: BitVec<u16, Msb0> = binary.view_bits::<Msb0>().to_bitvec();
+        return bitvector;
+    }
+
+    fn convert_to_binary(decimal: u16) -> u16 {
+        let binary: u16 = format!("{:b}", decimal).parse().unwrap();
+        return binary;
+    }
+
+    fn get_group_vector(data: String) -> Vec<u16> {
+        let numeric_groups: NumericGroups = NumericGroups::new(data);
+        return numeric_groups.break_string_to_group(); 
     }
 }
