a5fe6c3007a4f12477fd37082613c3e97c2f3536
TimurBora <timurborisov5561@gmail.com>
2023-07-15 22:21:18 +0200

    Okey

--- src/char_counter_builder.rs
+++ src/char_counter_builder.rs
@@ -3,12 +3,12 @@ use bitvec::prelude::*;
 
 use crate::append_to_bitvec;
 
-fn get_bitvector_char_counter(bitvector: &mut BitVec, character: String) {
+pub fn get_bitvector_char_counter(character: String) -> BitVec {
     let mut bitvec: BitVec = BitVec::new();
     bitvec.reserve(10);
 
-    let character_len: u32 =  character.len();
-    append_to_bitvec(&bitvec, character_len, 10);
+    let character_len: u32 = character.len().try_into().unwrap();
+    append_to_bitvec(&mut bitvec, &character_len, 10);
         
     return bitvec;
 }
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod numeric_data_operations;
 mod filling_data_vectors;
 mod encode_data_to_matrix;
 mod ZigZagIt;
+mod char_counter_builder;
 
 use qr_matrix::QrMatrix;
 use finder_builder::FinderBuilder;
@@ -19,6 +20,7 @@ use numeric_data_operations::NumericToBinaryConverter;
 use bitvec::prelude::*;
 use filling_data_vectors::add_bits_to_required_len;
 use encode_data_to_matrix::DataEncoder;
+use char_counter_builder::get_bitvector_char_counter;
 
 fn append_to_bitvec(bitvec: &mut BitVec, integer: &u32, bit_len: usize) {
     bitvec.extend((0..bit_len).rev().map(|i| (integer >> i) & 1 != 0));
@@ -46,15 +48,19 @@ fn main() {
     let mut timing_builder: TimingBuilder = TimingBuilder::new(&mut qr_block);
     timing_builder.add_timing_blocks();
 
-    // let mode: Mode = Mode::Byte;
-    // mode.add_mode_to_qr_matrix(&mut qr_block);
+    let mut bitvec: BitVec = BitVec::new();
+    bitvec.append(&mut Mode::get_bitvec(&Mode::Numeric));
 
+    bitvec.append(&mut get_bitvector_char_counter("123123".to_string()));
+    
     let numeric_binary_convert: NumericToBinaryConverter = NumericToBinaryConverter::new("123123".to_string());
     let mut numeric_bitvector = numeric_binary_convert.merge_bit_vectors();
 
     add_bits_to_required_len(&mut numeric_bitvector);
 
-    let mut data_encoder: DataEncoder = DataEncoder::new(numeric_bitvector, &mut qr_block);
+    bitvec.append(&mut numeric_bitvector);
+
+    let mut data_encoder: DataEncoder = DataEncoder::new(bitvec.clone(), &mut qr_block);
     data_encoder.encode_to_matrix();
 
     qr_block.print_matrix();
