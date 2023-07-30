6a082e2a482e13cd22afac3a8641682e04da4ede
TimurBora <timurborisov5561@gmail.com>
2023-07-30 19:01:18 +0200

    ECCLevel

--- src/error_correction_level.rs
+++ src/error_correction_level.rs
@@ -3,20 +3,13 @@
 use bitvec::prelude::*;
 
 pub enum ECCLevel {
-    L,
+    L = 0,
     M,
     Q,
     H,
 }
 
 impl ECCLevel {
-    fn get_error_correction_bitvec(&self) -> BitVec {
-        match self {
-            Self::L => return bitvec![0, 1],
-            Self::M => return bitvec![0, 0],
-            Self::Q => return bitvec![1, 1],
-            Self::H => return bitvec![1, 0],
-        }
-    }
+    
 }
 
